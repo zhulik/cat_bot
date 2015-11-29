2dee00f5441602ff851cd2065b0e122bfb5a4919
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2015-11-29 13:48:41 +0300

    Improvements

--- config_session.go
+++ config_session.go
@@ -12,35 +12,25 @@ type ConfigSession struct {
 func (session ConfigSession) HandleResponse(bot margelet.MargeletAPI, message tgbotapi.Message, responses []string) (bool, error) {
 	switch len(responses) {
 	case 0:
-		msg := tgbotapi.MessageConfig{Text: "Would you like to receive a cat images? (yes/no)"}
-		msg.ReplyMarkup = tgbotapi.ForceReply{true, true}
-		msg.ChatID = message.Chat.ID
-		msg.ReplyToMessageID = message.MessageID
-		bot.Send(msg)
+		msg := tgbotapi.MessageConfig{Text: "Would you like to receive a cat images sometimes? (yes/no)"}
+		session.forceReply(bot, message, msg)
 		return false, nil
 	case 1:
 		if message.Text != "yes" && message.Text != "no" {
 			msg := tgbotapi.MessageConfig{Text: "Sorry, i can't understand, type yes or no"}
-			msg.ReplyMarkup = tgbotapi.ForceReply{true, true}
-			msg.ChatID = message.Chat.ID
-			msg.ReplyToMessageID = message.MessageID
-			bot.Send(msg)
+			session.forceReply(bot, message, msg)
 			return false, errors.New("Waiting for yes or no message")
 		}
 
 		if message.Text == "yes" {
 			bot.GetConfigRepository().Set(message.Chat.ID, "yes")
 			msg := tgbotapi.MessageConfig{Text: "Ok, i will send you a cat sometimes"}
-			msg.ChatID = message.Chat.ID
-			msg.ReplyToMessageID = message.MessageID
-			bot.Send(msg)
+			session.reply(bot, message, msg)
 			return true, nil
 		} else {
 			bot.GetConfigRepository().Set(message.Chat.ID, "no")
 			msg := tgbotapi.MessageConfig{Text: "Ok, i will not send you a cat sometimes"}
-			msg.ChatID = message.Chat.ID
-			msg.ReplyToMessageID = message.MessageID
-			bot.Send(msg)
+			session.reply(bot, message, msg)
 			return true, nil
 		}
 	}
@@ -49,4 +39,15 @@ func (session ConfigSession) HandleResponse(bot margelet.MargeletAPI, message tg
 
 func (responder ConfigSession) HelpMessage() string {
 	return "Configure bot"
+}
+
+func (session ConfigSession) forceReply(bot margelet.MargeletAPI, message tgbotapi.Message, msg tgbotapi.MessageConfig) {
+	msg.ReplyMarkup = tgbotapi.ForceReply{true, true}
+	session.reply(bot, message, msg)
+}
+
+func (session ConfigSession) reply(bot margelet.MargeletAPI, message tgbotapi.Message, msg tgbotapi.MessageConfig) {
+	msg.ChatID = message.Chat.ID
+	msg.ReplyToMessageID = message.MessageID
+	bot.Send(msg)
 }

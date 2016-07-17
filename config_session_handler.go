55170715d223c107424c25a616928d93c4862d2a
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2016-07-17 14:41:27 +0300

    New margelet API compatibility

--- config_session_handler.go
+++ config_session_handler.go
@@ -3,20 +3,18 @@ package main
 import (
 	"errors"
 	"github.com/zhulik/margelet"
-	"gopkg.in/telegram-bot-api.v4"
 )
 
 type ConfigSessionHandler struct {
 }
 
-func (session ConfigSessionHandler) HandleSession(bot margelet.MargeletAPI, message *tgbotapi.Message, responses []tgbotapi.Message) (bool, error) {
-	switch len(responses) {
+func (s ConfigSessionHandler) HandleSession(bot margelet.MargeletAPI, session margelet.Session) error {
+	switch len(session.Responses()) {
 	case 0:
-		msg := tgbotapi.MessageConfig{Text: "Would you like to receive a cat images sometimes? (yes/no)"}
-		session.forceReply(bot, message, msg)
-		return false, nil
+		session.QuickForceReply("Would you like to receive a cat images sometimes? (yes/no)")
+		return nil
 	default:
-		return session.handleResponse(bot, message)
+		return s.handleResponse(bot, session)
 	}
 }
 
@@ -24,36 +22,24 @@ func (responder ConfigSessionHandler) HelpMessage() string {
 	return "Configure bot"
 }
 
-func (session ConfigSessionHandler) handleResponse(bot margelet.MargeletAPI, message *tgbotapi.Message) (bool, error) {
-	if message.Text != "yes" && message.Text != "no" {
-		msg := tgbotapi.MessageConfig{Text: "Sorry, i can't understand, type yes or no"}
-		session.forceReply(bot, message, msg)
-		return false, errors.New("Waiting for yes or no message")
+func (s ConfigSessionHandler) handleResponse(bot margelet.MargeletAPI, session margelet.Session) error {
+	if session.Message().Text != "yes" && session.Message().Text != "no" {
+		session.QuickForceReply("Sorry, i can't understand, type yes or no")
+		return errors.New("Waiting for yes or no message")
 	}
 
-	if message.Text == "yes" {
-		bot.GetConfigRepository().Set(message.Chat.ID, "yes")
-		msg := tgbotapi.MessageConfig{Text: "Ok, i will send you a cat sometimes"}
-		session.reply(bot, message, msg)
-		return true, nil
+	if session.Message().Text == "yes" {
+		bot.GetConfigRepository().Set(session.Message().Chat.ID, "yes")
+		session.QuickReply("Ok, i will send you a cat sometimes")
+		session.Finish()
+		return nil
 	} else {
-		bot.GetConfigRepository().Set(message.Chat.ID, "no")
-		msg := tgbotapi.MessageConfig{Text: "Ok, i will not send you a cat sometimes"}
-		session.reply(bot, message, msg)
-		return true, nil
+		bot.GetConfigRepository().Set(session.Message().Chat.ID, "no")
+		session.QuickReply("Ok, i will not send you a cat sometimes")
+		session.Finish()
+		return nil
 	}
 }
 
-func (session ConfigSessionHandler) forceReply(bot margelet.MargeletAPI, message *tgbotapi.Message, msg tgbotapi.MessageConfig) {
-	msg.ReplyMarkup = tgbotapi.ForceReply{true, true}
-	session.reply(bot, message, msg)
-}
-
-func (session ConfigSessionHandler) reply(bot margelet.MargeletAPI, message *tgbotapi.Message, msg tgbotapi.MessageConfig) {
-	msg.ChatID = message.Chat.ID
-	msg.ReplyToMessageID = message.MessageID
-	bot.Send(msg)
-}
-
-func (session ConfigSessionHandler) CancelSession(bot margelet.MargeletAPI, message *tgbotapi.Message, responses []tgbotapi.Message) {
+func (s ConfigSessionHandler) CancelSession(bot margelet.MargeletAPI, session margelet.Session) {
 }

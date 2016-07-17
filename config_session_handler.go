57b384f2d87795240fca0827314516773d97544d
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2016-07-17 15:37:06 +0300

    margelet API compatibility fix

--- config_session_handler.go
+++ config_session_handler.go
@@ -8,13 +8,13 @@ import (
 type ConfigSessionHandler struct {
 }
 
-func (s ConfigSessionHandler) HandleSession(bot margelet.MargeletAPI, session margelet.Session) error {
+func (s ConfigSessionHandler) HandleSession(session margelet.Session) error {
 	switch len(session.Responses()) {
 	case 0:
 		session.QuickForceReply("Would you like to receive a cat images sometimes? (yes/no)")
 		return nil
 	default:
-		return s.handleResponse(bot, session)
+		return s.handleResponse(session)
 	}
 }
 
@@ -22,24 +22,24 @@ func (responder ConfigSessionHandler) HelpMessage() string {
 	return "Configure bot"
 }
 
-func (s ConfigSessionHandler) handleResponse(bot margelet.MargeletAPI, session margelet.Session) error {
+func (s ConfigSessionHandler) handleResponse(session margelet.Session) error {
 	if session.Message().Text != "yes" && session.Message().Text != "no" {
 		session.QuickForceReply("Sorry, i can't understand, type yes or no")
 		return errors.New("Waiting for yes or no message")
 	}
 
 	if session.Message().Text == "yes" {
-		bot.GetConfigRepository().Set(session.Message().Chat.ID, "yes")
+		session.GetConfigRepository().Set(session.Message().Chat.ID, "yes")
 		session.QuickReply("Ok, i will send you a cat sometimes")
 		session.Finish()
 		return nil
 	} else {
-		bot.GetConfigRepository().Set(session.Message().Chat.ID, "no")
+		session.GetConfigRepository().Set(session.Message().Chat.ID, "no")
 		session.QuickReply("Ok, i will not send you a cat sometimes")
 		session.Finish()
 		return nil
 	}
 }
 
-func (s ConfigSessionHandler) CancelSession(bot margelet.MargeletAPI, session margelet.Session) {
+func (s ConfigSessionHandler) CancelSession(session margelet.Session) {
 }
